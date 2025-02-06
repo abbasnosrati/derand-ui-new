8b8539db0b4b6b5885ba48da4f72419f5fb788ce
abbasnosrati <abbas.nosrati68@gmail.com>
2025-02-06 12:12:01 +0330

    move img

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,6 @@ function App() {
   return (
     <div className="page-noise ">
       <div className="content relative flex items-center justify-center">
-        <img
-          className="absolute w-[100%] top-0"
-          src="./assets/images/header/topLeftLight.svg"
-        />
         <div className="max-w-[1440px] above-1440:max-w-[1920px] w-full">
           <Header />
           {/* <Example /> */}
